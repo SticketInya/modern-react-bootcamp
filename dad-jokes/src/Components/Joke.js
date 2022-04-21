cb2c89fbce42b9e9b69074c3f18a6fffdff4b29a
JPatrik <jokhel.roland@gmail.com>
2022-04-21 14:53:28 +0200

    style(*): added Joke component styling

--- dad-jokes/src/Components/Joke.js
+++ dad-jokes/src/Components/Joke.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import '../Styles/Joke.css';
 
 class Joke extends Component {
     render() {
         return (
             <div className='Joke'>
-                <div className='Joke__voteContainer'>
-                    <i className='fa fa-arrow-up' onClick={this.props.upVote} />
+                <div className='Joke__votesContainer'>
+                    <i
+                        className='Joke__btn fa fa-arrow-up'
+                        onClick={this.props.upVote}
+                    />
                     <div className='Joke__votes'>{this.props.votes}</div>
                     <i
-                        className='fa fa-arrow-down'
+                        className='Joke__btn fa fa-arrow-down'
                         onClick={this.props.downVote}
                     />
                 </div>
                 <div className='Joke__text'>{this.props.joke}</div>
+                <div className='Joke__emote'>
+                    <span class='ec ec-rofl'></span>
+                </div>
             </div>
         );
     }
